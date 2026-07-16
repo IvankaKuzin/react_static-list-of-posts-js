8c05324a3de8aef1c3a156a2f4cb2024a0cf9470
IvankaKuzin <kuziniwanka@gmail.com>
2026-07-16 22:32:17 +0300

    Solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,17 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import commentsFromServer from '../../api/comments.json';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ postId }) => {
+  const commentsForPost = commentsFromServer.filter(
+    comment => comment.postId === postId,
+  );
+
+  return (
+    <div className="CommentList">
+      {commentsForPost.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,26 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+import usersFromServer from '../../api/users.json';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <UserInfo
+          user={usersFromServer.find(user => user.id === post.userId)}
+        />
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
+    <CommentList postId={post.id} />
+  </div>
+);
